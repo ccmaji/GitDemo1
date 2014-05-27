4dd237cae3b994b3e763f00020351ee173270ab1
ccmaji <1071867675@qq.com>
2014-05-27 10:54:31 +0800

    update twice

--- Git.java
+++ Git.java
@@ -1,6 +1,6 @@
 public class Git{
 	public static void main(String args[]){
-		System.out.println("Hello world! It is beautiful master!!!");
+		System.out.println("Hello world! It is beautiful master!!! twice");
 	}
 
 }
